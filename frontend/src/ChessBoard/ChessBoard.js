57fdb3b9dfe5d3a51075e224667acbc49bc3595c
Yuxiang Chen <chenyuxiang0927@gmail.com>
2019-04-17 16:31:35 -0700

    finish drawing board

--- frontend/src/ChessBoard/ChessBoard.js
+++ frontend/src/ChessBoard/ChessBoard.js
@@ -3,14 +3,32 @@ import style from './ChessBoard.module.css';
 
 class ChessBoard extends Component {
 
+    board_size = 480;
+    interval = this.board_size / 16;
+
     componentDidMount() {
         const board = this.refs.board;
+        const ctx = board.getContext("2d");
+
+        for (let i = 1; i <= 15; i++) {
+            // Vertical Lines
+            ctx.beginPath();
+            ctx.strokeStyle = "#555"
+            ctx.moveTo(this.interval * i, this.interval);
+            ctx.lineTo(this.interval * i, this.board_size - this.interval);
+            ctx.stroke();
+            
+            ctx.strokeStyle = "#555"
+            ctx.moveTo(this.interval, this.interval * i);
+            ctx.lineTo(this.board_size - this.interval, this.interval * i);
+            ctx.stroke();
+        }
     }
 
     render() {
         return (
             <div className="ChessBoard">
-                <canvas className={style.board} ref="board" width={480} height={480} />
+                <canvas className={style.board} ref="board" width={this.board_size} height={this.board_size} />
             </div>
         );
     }
