77eb8f777400e8dc6230113fa82deb5d7e66cefd
Tianyu Cao <tianyu.cao@sjsu.edu>
2019-04-17 19:39:38 -0700

    login frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ChessBoard from './ChessBoard/ChessBoard';
+import LoginPage from './LoginPage/LoginPage'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <LoginPage></LoginPage>
         <ChessBoard></ChessBoard>
       </div>
     );
---
+++ frontend/src/LoginPage/LoginPage.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class LoginPage extends Component {
+    username = "abc"
+    attempt = 5;
+
+    state = {
+        username: "",
+        password: ""
+    }
+
+    onChangeHandler = (e) => {
+        console.log(e);
+        this.setState({
+            [e.target.id]: e.target.value
+        });
+    }
+
+    onLoginHandler = () => {
+        console.log("Username:", this.state.username);
+        console.log("Password:", this.state.password);
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <h1>Login</h1>
+                <input type="text" id="username" value={this.state.username} onChange={this.onChangeHandler}></input>
+                <br></br>
+                <input type="text" id="password" value={this.state.password} onChange={this.onChangeHandler}></input>
+                <br></br>
+                <input type="button" value="Login" onClick={this.onLoginHandler}></input>
+            </React.Fragment>
+
+        );
+    }
+}
+
+export default LoginPage;
