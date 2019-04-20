cc8e0d9c73f98e1c0d58c29c1b5f12c94910d898
Yuxiang Chen <chenyuxiang0927@gmail.com>
2019-04-19 21:59:16 -0700

    adding socket

--- backend/tetris/src/main/java/com/goofyobject/tetris/WebSocketConfig.java
+++ backend/tetris/src/main/java/com/goofyobject/tetris/WebSocketConfig.java
@@ -18,7 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-game").withSockJS();
+        registry.addEndpoint("/ws-game").setAllowedOrigins("*").withSockJS();
     }
 
 }
