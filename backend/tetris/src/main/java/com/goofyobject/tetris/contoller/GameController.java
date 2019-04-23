dea2834d257e2949e8ea5f58664df25ab47dbaa2
Chi <ckloi@ucdavis.edu>
2019-04-22 18:53:43 -0700

    add request mapping

--- backend/tetris/src/main/java/com/goofyobject/tetris/contoller/GameController.java
+++ backend/tetris/src/main/java/com/goofyobject/tetris/contoller/GameController.java
@@ -20,9 +20,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @CrossOrigin("*")
+@RequestMapping()
 public class GameController {
 
     @Autowired
