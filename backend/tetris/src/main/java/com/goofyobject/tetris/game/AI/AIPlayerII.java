81666029879cb18156ff8600381127ded1434548
Tianyu Cao <tianyu.cao@sjsu.edu>
2019-04-29 23:03:49 -0700

    add setChess

--- backend/tetris/src/main/java/com/goofyobject/tetris/game/AI/AIPlayerII.java
+++ backend/tetris/src/main/java/com/goofyobject/tetris/game/AI/AIPlayerII.java
@@ -29,4 +29,28 @@ public class AIPlayerII implements Data {
         setChess();
     }
 
+    public void setChess() {
+
+        /** Black chess */
+        hm.put("1", 12);
+        hm.put("11", 120);
+        hm.put("111", 1200);
+        hm.put("1111", 10000);
+        hm.put("12", 10);
+        hm.put("112", 100);
+        hm.put("1112", 1000);
+        hm.put("11112", 10000);
+
+        /** White chess */
+        hm.put("2", 21);
+        hm.put("22", 210);
+        hm.put("222", 2100);
+        hm.put("2222", 20000);
+        hm.put("21", 20);
+        hm.put("221", 200);
+        hm.put("2221", 2000);
+        hm.put("22221", 20000);
+    }
+
+    
 }
