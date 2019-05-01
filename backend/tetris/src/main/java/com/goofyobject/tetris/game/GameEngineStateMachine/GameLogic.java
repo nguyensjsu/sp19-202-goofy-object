e380d3d0a2fe72ee4e5bc12ba465f10cfbd22598
504steven <504steven@gmail.com>
2019-05-01 11:35:42 -0700

    update gamelogic

--- backend/tetris/src/main/java/com/goofyobject/tetris/game/GameEngineStateMachine/GameLogic.java
+++ backend/tetris/src/main/java/com/goofyobject/tetris/game/GameEngineStateMachine/GameLogic.java
@@ -2,6 +2,7 @@ package com.goofyobject.tetris.game.GameEngineStateMachine;
 
 import com.goofyobject.tetris.game.AI.AIPlayerI;
 import com.goofyobject.tetris.game.entity.Board;
+import com.goofyobject.tetris.game.entity.Piece;
 import com.goofyobject.tetris.game.entity.Position;
 
 public class GameLogic {
@@ -44,7 +45,7 @@ public class GameLogic {
     }
 
     public String checkWinner(Position p) {
-        int res = this.board.checkFiveInRow(p);
+        int res = this.checkFiveInRow(p);
         if(res == 1) {
             this.curState = this.gameEndState;
             return playerOneMoveState.getPlayerId();
@@ -57,7 +58,7 @@ public class GameLogic {
     }
 
     public boolean checkDraw() {
-        boolean res = this.board.checkDraw();
+        boolean res = this.board.isFull();
         if(res) {
             this.curState = this.gameEndState;
         }
@@ -70,5 +71,123 @@ public class GameLogic {
 
     public String getId2() {
         return this.playerTwoMoveState.getPlayerId();
+    }
+
+
+    // 1 -p1 win, 2- p2 win, 0- no win
+    private int checkFiveInRow(Position p) {
+        if(p == null) { return 0;}
+        Piece[][] grid = this.board.getGrid();
+        int gridNum = this.board.getGridNum();
+        int x = p.getX();
+        int y = p.getY();
+        if(x < 0 || y < 0 || x >= gridNum || y >= gridNum) {    return 0;}
+        Piece cur = grid[x][y];
+        if(cur == null) { return 0;}
+        int curColor = cur.getColor();
+        int count = 1;
+
+        // check left
+        int i = 1;
+        while(x-i >= 0 && i < 5) {
+            if(grid[x-i][y] != null && grid[x-i][y].getColor() == curColor) {
+                count++;
+                i++;
+            }else {
+                break;
+            }
+        }
+        if(count >= 5) {    return curColor;}
+
+        // check right
+        i = 1;
+        while(x+i < gridNum && i < 5) {
+            if(grid[x+i][y] != null && grid[x+i][y].getColor() == curColor) {
+                count++;
+                i++;
+            }else {
+                break;
+            }
+        }
+        if(count >= 5) {    return curColor;}
+
+        count = 1;
+        // check up
+        i = 1;
+        while(y-i >= 0  && i < 5) {
+            if(grid[x][y-i] != null && grid[x][y-i].getColor() == curColor) {
+                count++;
+                i++;
+            }else {
+                break;
+            }
+        }
+        if(count >= 5) {    return curColor;}
+
+        // check down
+        i = 1;
+        while(y+i < gridNum && i < 5) {
+            if(grid[x][y+i] != null && grid[x][y+i].getColor() == curColor) {
+                count++;
+                i++;
+            }else {
+                break;
+            }
+        }
+        if(count >= 5) {    return curColor;}
+
+        count = 1;
+        // check right-down
+        i = 1;
+        while(x+i < gridNum && y+i < gridNum && i < 5) {
+            if(grid[x+i][y+i] != null && grid[x+i][y+i].getColor() == curColor) {
+                count++;
+                i++;
+            }else {
+                break;
+            }
+        }
+        if(count >= 5) {    return curColor;}
+
+        // check left-up
+        i = 1;
+        while(x-i >= 0 && y-i >= 0 && i < 5) {
+            if(grid[x-i ][y-i ] != null && grid[x-i ][y-i ].getColor() == curColor) {
+                count++;
+                i++;
+            }else {
+                break;
+            }
+        }
+        if(count >= 5) {    return curColor;}
+
+
+        count = 1;
+        // check left-down
+        i = 1;
+        while(x-i >= 0 && y+i < gridNum && i < 5) {
+            if(grid[x-i][y+i] != null && grid[x-i][y+i].getColor() == curColor) {
+                count++;
+                i++;
+            }else {
+                break;
+            }
+        }
+        if(count >= 5) {    return curColor;}
+
+
+        // check right-up
+        i = 1;
+        while(x+i < gridNum && y-i >= 0 && i < 5) {
+            if(grid[x+i][y-i ] != null && grid[x+i][y-i ].getColor() == curColor) {
+                count++;
+                i++;
+            }else {
+                break;
+            }
+        }
+        if(count >= 5) {    return curColor;}
+
+        return 0;
     }
 }
