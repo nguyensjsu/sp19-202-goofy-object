cb0f3738080ca7a04f69f10009d326d986a156ec
Chi <ckloi@ucdavis.edu>
2019-05-01 00:02:47 -0700

    change back to cloud

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 var config = {};
 
- // config.host = "https://five-in-row-202.herokuapp.com";
- config.host = "http://localhost:8080";
+config.host = "https://five-in-row-202.herokuapp.com";
+//  config.host = "http://localhost:8080";
 
 export default config;
