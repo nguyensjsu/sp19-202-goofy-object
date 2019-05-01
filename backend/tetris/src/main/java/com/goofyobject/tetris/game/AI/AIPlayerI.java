b432b6738a01370353964f39add9464bb266c56a
Xiaoting Jin <sharon.jin0524@gmail.com>
2019-05-01 01:39:01 -0700

    t

--- backend/tetris/src/main/java/com/goofyobject/tetris/game/AI/AIPlayerI.java
+++ backend/tetris/src/main/java/com/goofyobject/tetris/game/AI/AIPlayerI.java
@@ -1,6 +1,7 @@
 package com.goofyobject.tetris.game.AI;
 
 import com.goofyobject.tetris.game.entity.Board;
+import com.goofyobject.tetris.game.entity.Piece;
 import com.goofyobject.tetris.game.entity.Position;
 
 // https://zjh776.iteye.com/blog/1979748
@@ -13,6 +14,10 @@ import java.util.Random;
 // human forever black   1
 public class AIPlayerI {
     private Board board;
+    private int aiColor = 2; //computer fixed to white
+    private final int gridNum = 15;
+
+
 
     public AIPlayerI(Board board) {
         this.board = board;
@@ -30,4 +35,28 @@ public class AIPlayerI {
         Position result = new Position(x, y);
         return result;
     }
+
+    // actual method
+    public Position getComputerPosition_2() {
+
+        int maxComputerScore = 0;
+        Piece[][] grid = board.getGrid();
+        // if the board is full, there's no pisition to place piece
+        Position result = new Position(100, 100);
+
+        // return the position with maximum Computer score
+        for (int i = 0; i < gridNum; i++) {
+            for (int j = 0; j < gridNum; j++) {
+                if (grid[i][j] == null && maxComputerScore < gridScore[i][j]) {
+                    maxComputerScore = gridScore[i][j];
+                    result = new Position(i, j);
+                    computerScore = maxComputerScore;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+
 }
