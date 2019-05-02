6cbfaf738204336fd63ee6f72154ab0fd412d881
Yuxiang Chen <chenyuxiang0927@gmail.com>
2019-05-01 22:28:15 -0700

    add password in login

--- frontend/src/LoginPage/LoginPage.js
+++ frontend/src/LoginPage/LoginPage.js
@@ -7,6 +7,7 @@ class LoginPage extends Component {
 
     state = {
         username: "",
+        password: "",
         msg: ""
     }
 
@@ -20,7 +21,8 @@ class LoginPage extends Component {
     onLoginHandler = () => {
         console.log("Username:", this.state.username);
         axios.post(config.host + "/login", {
-            username: this.state.username
+            username: this.state.username,
+            password: this.state.password
         }).then(res => {
             if (res.status === 200) {
                 console.log('Login status:', res.data);
@@ -46,7 +48,9 @@ class LoginPage extends Component {
             <React.Fragment>
                 <h1>Login</h1>
                 <label htmlFor="username">Username: </label>
-                <input type="text" id="username" value={this.state.username} onChange={this.onChangeHandler}></input>
+                <input type="text" id="username" value={this.state.username} onChange={this.onChangeHandler}></input><br/>
+                <label htmlFor="password">Password: </label>
+                <input type="text" id="password" value={this.state.password} onChange={this.onChangeHandler}></input><br/>
                 <input type="button" value="Login" onClick={this.onLoginHandler}></input>
                 <input type="button" value="New User" onClick={this.onNewUserHandler}></input>
                 <p>{this.state.msg}</p>
