0c4df1d0ae4edda6c37f979b3d94e9dcf3dfd9f6
Yuxiang Chen <chenyuxiang0927@gmail.com>
2019-05-01 21:52:30 -0700

    adding new page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Route, Link, Redirect, Switch } from 'react-ro
 import ChessBoard from './ChessBoard/ChessBoard';
 import LoginPage from './LoginPage/LoginPage';
 import ModeSelection from './ModeSelection/ModeSelection';
+import RegisterPage from './RegisterPage/RegisterPage';
 
 class App extends Component {
 
@@ -28,9 +29,10 @@ class App extends Component {
           <h1>Five Goofy in a Row</h1>
           <Switch>
             <Route path="/login" render={(props) => <LoginPage {...props} onLoginHandler={this.onLoginHandler} />} />
+            <Route path="/register" component={RegisterPage} />
+            {this.state.loggedIn ? null : <Redirect to="/login"></Redirect>}
             <Route path="/mode" component={ModeSelection} />
             {this.state.loggedIn ? <Route path="/game" component={ChessBoard} /> : null}
-            {this.state.loggedIn ? null : <Redirect to="/login"></Redirect>}
           </Switch>
         </div>
       </Router>
