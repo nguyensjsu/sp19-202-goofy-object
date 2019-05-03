6350a242533307a81bf2bc5ea64d107d3c04194e
Chi <ckloi@ucdavis.edu>
2019-05-03 12:48:20 -0700

    merge

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 var config = {};
 
 
- config.host = "https://five-in-row-202.herokuapp.com";
-// config.host = "http://localhost:8080";
+//  config.host = "https://five-in-row-202.herokuapp.com";
+config.host = "http://localhost:8080";
 
 export default config;
